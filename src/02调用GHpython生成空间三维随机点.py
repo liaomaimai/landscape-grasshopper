e07321d72d9fdcec6f715e33369bbc024286e73e
Magic Mai <498269108@qq.com>
2024-06-06 01:49:15 +0800

    brq

--- src/02调用GHpython生成空间三维随机点.py
+++ src/02调用GHpython生成空间三维随机点.py
@@ -1,60 +1,103 @@
-import rhinoscriptsyntax as rs
-import ghpythonlib.components as ghc
-import random
-import itertools
-import ghpythonlib.treehelpers as th
-import Rhino.Geometry as rg
+"""Provides a scripting component.
+   Inputs:
+       pt_s: 位置点（方向起始点）。Data structure：Item Access;Type hint：Point3D
+       pt_e: 方向结束点。Data structure：Item Access;Type hint：Point3D
+       Distance: 点间距离。Data structure：Item Access;Type hint：Default
+       Count: 点数量（一边）。Data structure：Item Access;Type hint：int
+       Rotate: 面间旋转角度。Data structure：Item Access;Type hint：Default
+       Ratio_seed： 变化随机种子。Data structure：Item Access;Type hint：Default
+   Output:
+       spaceStrussCube_pts: 空间立方体结构随机变化点
+       spaceStrussCube_crvs:空间立方体结构随机变化曲线
+       """
+
+__author__ = "richiebao caDesign设计(cadesign.cn)"
+__version__ = "2022.09.14"
+
+ghenv.Component.Name = 'space truss cube pts'
+ghenv.Component.NickName = 'space truss cube pts'
+ghenv.Component.Description = '随机变化空间立方体结构'
+ghenv.Component.Message = '0.0.1'
+ghenv.Component.Category = 'Moths'
+ghenv.Component.SubCategory = 'Database'
+ghenv.Component.AdditionalHelpFromDocStrings = '1'
+
 from Grasshopper import DataTree
 from Grasshopper.Kernel.Data import GH_Path
+import Rhino.Geometry as rg
+import ghpythonlib.components as ghc
+import ghpythonlib.treehelpers as th
+import random
+import itertools
 
-#定义函数，展开list
-flatten_lst=lambda lst:[m for n_lst in lst  for m in flatten_lst(n_lst)] if type(lst) is list else [lst]
-#lambda表达式，定义了一个隐藏函数。
-#def flatten)lst(lst):
-#   return [m for n_lst in lst  for m in flatten_lst(n_lst)] if type(lst) is list else [lst]
-
-def space_truss_cube_pts(pt_start,pt_end,distance,count):
-    #建立空间立方体结构，并产生随机变化的点
-
-    #建立X方向单位向量
-    vector_x=ghc.Vector2Pt(pt_start,pt_end,True)[0]
-    #生成同方向，不同倍数距离的向量
-    vectors4XMove=[ghc.Amplitude(vector_x,distance*i) for i in range(count)]
-    #将起点沿上方向量列表依次移动
-    pts_x=[ghc.Move(pt_start,v) for v in vectors4XMove]
-
-    
-    #建立Y方向的单位向量
-    vector_y=ghc.Vector2Pt(pt_start,ghc.Rotate(pt_end,ghc.Radians(90),ghc.XYPlane(pt_start)[0]))[0]
-    #生成同Y方向，不同倍数距离向量
-    vectors4YMove=[ghc.Amplitude(vector_y,distance*i) for i in range(count)]
-    #根据X方向点，生成Y方向点
-    pts_xy=[[ghc.Move(pt,v) for v in vectors4YMove] for pt in pts_x]
-
-    #建立Z方向单位向量
-    vector_z=ghc.UnitZ(distance)
-    #生成同Z方向，不同倍数距离向量
-    vectors4ZMove=[ghc.Amplitude(vector_z,distance*i) for i in range(count)]
-    #根据XY点集，生成三维点集
-    pts_xyz=[[[ghc.Move(pt_y,v) for v in vector_z] for pt_y in pts_x] for pts_x in pts_xy]
-    #拍平数据
-    pts_xyz_tree=th.list_to_tree(pts_xyz)
+flatten_lst=lambda lst: [m for n_lst in lst for m in flatten_lst(n_lst)] if type(lst) is list else [lst]
 
 
-    #随机半径球体上随机点
-    random_radius=map(lambda x:x*distance/2,ghc.Random(ghc.ConstructDomain(0,1),len(flatten_lst(pts_xyz)),Ratio_seed))#map(a,b)函数，a为函数，B为迭代器
-    random_sphere=[ghc.Sphere(ghc.XYPlane(pt),r)for pt,r in zip([p  for p in pts_xyz_tree.Branches],random_radius)]
-    random_pts=[ghc.PopulateGeometry(s,1,random.uniform(0,1000)) for s in random_sphere]
+def space_truss_cube_pts(pt_s,pt_e,Distance,Count,):
+   '''
+   建立空间立方体结构随机变化点
 
+   Parameters
+   ----------
+   pt_s : Point3D
+       位置点（方向起始点）.
+   pt_e : Point3D
+       方向结束点.
+   Distance : float
+       点间距离.
+   Count : int
+       点数量（一边）.
 
-    #转换tree数据结构
-    random_pts_tree=DataTree[rg.Point3d]()
-    for pt, path in zip(random_pts, pts_xyz_tree.Paths):
-        random_pts_tree.Add(pt, path)
-    return random_pts_tree
+   Returns
+   -------
+   random_pts_tree : Tree(Point3D)
+       空间立方体结构随机变化点.
 
+   '''
+   #A-X向点（单排）
+   vector_x=ghc.Vector2Pt(pt_s,pt_e,True)[0]
+   vectors4Xmove=[ghc.Amplitude(vector_x,Distance*i) for i in range(Count)]
+   pts_x=[ghc.Move(pt_s,v)[0] for v in vectors4Xmove]
+   
+   #B-Y向点（多排）
+   vector_y=ghc.Vector2Pt(pt_s,ghc.Rotate(pt_e,ghc.Radians(Rotate),ghc.XYPlane(pt_s))[0],True)[0]
+   vectors4Ymove=[ghc.Amplitude(vector_y,Distance*i) for i in range(Count)]
+   pts_xy=[[ghc.Move(pt,v)[0] for v in vectors4Ymove] for pt in pts_x]
+   
+   #C-Z向点（三维矩阵）
+   vector_z=ghc.UnitZ(Distance)
+   vectors4Zmove=[ghc.Amplitude(vector_z,Distance*i) for i in range(Count)]
+   pts_xyz=[[[ghc.Move(pt_y,v) for v in vectors4Zmove] for pt_y in pts_x] for pts_x in pts_xy]
+   pts_xyz_tree=th.list_to_tree(pts_xyz)
+   
+   #D-随机半径球体上随机点
+   random_radius=map(lambda x:x*Distance/2,ghc.Random(ghc.ConstructDomain(0,1),len(flatten_lst(pts_xyz)),Ratio_seed))
+   random_sphere=[ghc.Sphere(ghc.XYPlane(pt),r) for pt,r in zip([p[0] for p in pts_xyz_tree.Branches],random_radius)]
+   random_pts=[ghc.PopulateGeometry(pt,1,random.uniform(0,10000)) for pt in random_sphere]
+   
+   #E-转换为Tree类型数据结构
+   random_pts_tree=DataTree[rg.Point3d]()
+   for pt,path in zip(random_pts,pts_xyz_tree.Paths):
+       random_pts_tree.Add(pt,path)
+   
+   return random_pts_tree
+   
 def space_truss_cube_crvs(pts_tree):
-    #A-y和z向折线。通过路径组织分组点
+   '''
+   建立空间立方体结构随机变化曲线
+
+   Parameters
+   ----------
+   pts_tree : Tree(Point3D)
+       空间立方体结构随机变化点.
+
+   Returns
+   -------
+   crvs_xyz_tree : Tree(Polyline)
+       空间立方体结构随机变化曲线.
+
+   '''
+   #A-y和z向折线。通过路径组织分组点
    paths=pts_tree.Paths  
    paths_idx_z=zip(['%s%s%s'%(p[0],p[1],p[2]) for p in paths],paths)
    paths_idx_y=zip(['%s%s%s'%(p[0],p[1],p[3]) for p in paths],paths)   
@@ -88,7 +131,7 @@ def space_truss_cube_crvs(pts_tree):
    return crvs_xyz_tree
    
 if __name__=="__main__":
-   spaceStrussCube_pts=space_truss_cube_pts(pt_start,pt_end,distance,count)
+   spaceStrussCube_pts=space_truss_cube_pts(pt_s,pt_e,Distance,Count)
    print('Finished the points calculation...')
    spaceStrussCube_crvs=space_truss_cube_crvs(spaceStrussCube_pts)
    print('Finished the curves calculation...')
